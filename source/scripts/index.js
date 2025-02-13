b61eb6167a52cd9a5de38260cffe1c2775bcda7f
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-02-13 22:25:57 +0700

    nouislider

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,27 @@
 /* в этот файл добавляет скрипты*/
+
+const slider = document.querySelector('.nouislider');
+
+const createSlider = () => {
+  noUiSlider.create(slider, {
+    start: [0, 900],
+    connect: [false, true, false],
+    range: {
+      min: 0,
+      max: 1000,
+    },
+    step: 1,
+    format:{
+      to: function (value) {
+        if (Number.isInteger(value)) {
+          return value.toFixed(0);
+        }
+        return value.toFixed(1);
+      },
+      from: function (value) {
+        return parseFloat(value);
+      },
+    },
+  });
+};
+createSlider();
