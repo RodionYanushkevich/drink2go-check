658ac9b179f41cf954df4ae41966f6ada637182b
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-02-14 15:32:24 +0700

    catalog styling

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -25,3 +25,33 @@ const createSlider = () => {
   });
 };
 createSlider();
+
+
+const cardButtons = document.querySelectorAll('.card__button');
+cardButtons.forEach((cardButton) => {
+  cardButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('клик по кнопке');
+  });
+
+});
+
+
+// так так так
+
+// const select = document.querySelector('.sort__select');
+// const arrow = document.querySelector('.sort__arrow');
+
+// select.addEventListener('click', () => {
+//   arrow.classList.toggle('sort__arrow--active');
+// });
+
+
+// select.addEventListener('change', () => {
+//   arrow.classList.remove('sort__arrow--active');
+//   console.log('asdasdas');
+// });
+
+// select.addEventListener('blur', () => {
+//   arrow.classList.remove('sort__arrow--active');
+// });
