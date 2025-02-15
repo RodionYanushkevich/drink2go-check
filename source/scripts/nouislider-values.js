0b9d6da5f6e9247f4133edd40c2626e49b5cc2a4
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-02-15 19:25:20 +0700

    add js

---
+++ source/scripts/nouislider-values.js
@@ -0,0 +1,35 @@
+const priceFieldset = document.querySelector('.price-fieldset');
+
+const inputs = priceFieldset.querySelectorAll('.price-fieldset__input');
+
+const slider = priceFieldset.querySelector('.nouislider');
+const [minPriceInput, maxPriceInput] = inputs;
+
+const createSlider = () => {
+  noUiSlider.create(slider, {
+    start: [0, 900],
+    connect: [false, true, false],
+    range: {
+      min: 0,
+      max: 1000,
+    },
+    step: 1,
+    format:{
+      to: function (value) {
+        return Math.round(value); // Округляем до целого числа
+      },
+      from: function (value) {
+        return parseFloat(value);
+      },
+    },
+  });
+};
+
+createSlider();
+
+slider.noUiSlider.on('update', (values) => {
+  minPriceInput .value = values[0];
+  maxPriceInput.value = values[1];
+});
+
+minPriceInput .value = '';
---
+++ source/scripts/pagination-prev-block-hide.js
@@ -0,0 +1,12 @@
+const catalogPaginationList = document.querySelector('.pagination__list');
+
+const paginationItem = catalogPaginationList.querySelector('.pagination__item:nth-child(2)');
+
+const paginationLinkPrevios = catalogPaginationList.querySelector('.pagination__link[href="#prev"]');
+
+console.log(paginationLinkPrevios);
+
+if (paginationItem.classList.contains('pagination__item--current')) {
+  paginationLinkPrevios.style.display = 'none';
+}
+
