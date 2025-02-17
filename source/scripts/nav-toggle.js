c66b3e70caecba50a8ab549294774ef20d9da8db
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-02-18 06:29:11 +0700

    save

--- source/scripts/nav-toggle.js
+++ source/scripts/nav-toggle.js
@@ -2,11 +2,13 @@ const navigationList = document.querySelector('.navigation__list');
 const buttonToggle = document.querySelector('.navigation__button');
 
 
-buttonToggle.onclick = function() {
+buttonToggle.addEventListener('click', () => {
   navigationList.classList.toggle('navigation__list--open');
   if (buttonToggle.classList.contains('navigation__button--cross')) {
     buttonToggle.classList.remove('navigation__button--cross');
   }else{
     buttonToggle.classList.add('navigation__button--cross');
   }
-};
+});
+
+
